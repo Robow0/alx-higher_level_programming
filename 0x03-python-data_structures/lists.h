e41be5fec0c1030f2fc60ac3f11bf00ccca64ea9
Robow0 <robowsalad@gmail.com>
2023-12-03 23:02:19 -0800

    updated

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -18,4 +18,13 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
+def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
+def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
+def multiple_returns(sentence):
+def max_integer(my_list=[]):
+def divisible_by_2(my_list=[]):
+def delete_at(my_list=[], idx=0):
+void print_python_list_info(PyObject *p);
 #endif /* LISTS_H */
