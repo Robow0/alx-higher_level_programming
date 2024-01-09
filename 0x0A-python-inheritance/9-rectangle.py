5a9c2a3cbc612cc0c0b9aeaa3972e1e26c944d5e
Robow0 <robowsalad@gmail.com>
2024-01-09 04:00:18 -0800

    done

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,46 +1,31 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """creates a class BaseGeometry"""
-    def area(self):
-        """calculate the area
-        Raise:
-            Exception: area is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates the value
-        Args:
-            name(str): name
-            value(int): value
-        Raises:
-            TypeError: if value is not type int
-            ValueError: if value is less than or equal to 0
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+"""
+    Rectangle class inheriting other class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """creates a subclass Rectangle of class BaseGeometry"""
+    """
+    class  that inherits from BaseGeometry
+    """
     def __init__(self, width, height):
-        """initialize attributes
-        Args:
-            width(int): width of Rectangle
-            height(int): height of Rectangle
         """
-        BaseGeometry.__init__(self)
+            Initialize rectangle from BaseGeometry
+        """
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
         self.__width = width
         self.__height = height
-        super().integer_validator("width", self.__width)
-        super().integer_validator("height", self.__height)
 
     def area(self):
-        """calculates area"""
+        """
+        define the area
+        """
         return self.__width * self.__height
 
     def __str__(self):
-        """prints string"""
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        """
+            Rectangle with str
+        """
+        return("[Rectangle] {}/{}".format(self.__width, self.__height))
