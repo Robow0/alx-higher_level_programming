8d9b38991ea5e159a03d62146e74c810b139f49b
Robow0 <robowsalad@gmail.com>
2024-01-11 05:48:22 -0800

    updated

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""define write file with two arguments"""
 
-def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+
+def write_file(filename="" text=""):
+    """ reads filename with utf-8 """
+
+    with open(filename,"w", encoding='utf-8') as f:
+        return f.write(text)
