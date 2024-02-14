f5cc211b5b7bec265a6e2204eddc95c0df3b233d
Robow Salad <133003008+Robow0@users.noreply.github.com>
2024-02-14 09:06:39 +0000

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,8 @@
--- Lists all shows in the database htbn_0d_tvshows without a genre inked.
--- Records are ordered by ascending tv_shows.title and tv_show_genre.genre.id.
-SELECT s.`title`, g.`genre.id`
+-- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
-	LEFT JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show.id`
-	WHERE g.`genre.id` IS NULL
-  ORDER BY s.`title`, g.`genre.id`;
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
